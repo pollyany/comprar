e4385f6bba48ae9309e9ac01ee70d8e52100789a
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-07 17:03:18 -0300

    Lista de Itens

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -7,6 +7,23 @@ import { Filter } from "@/components/Filter";
 import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "1 pacote de café",
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "3 pacotes de macarrão",
+  },
+  {
+    id: "3",
+    status: FilterStatus.PENDING,
+    description: "3 cebolas",
+  },
+];
 
 export function Home() {
   return (
@@ -25,12 +42,25 @@ export function Home() {
           <TouchableOpacity style={styles.clearButton}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
-          <Item
-            data={{ status: FilterStatus.DONE, description: "Café" }}
-            onStatus={() => console.log("mudar o status")}
-            onRemove={() => console.log("remover")}
-          />
         </View>
+
+        <FlatList
+          data={ITEMS}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item}
+              onStatus={() => console.log("mudar o status")}
+              onRemove={() => console.log("remover")}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => (
+            <Text style={styles.empty}>Nenhum item aqui.</Text>
+          )}
+        />
       </View>
     </View>
   );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,5 +2,11 @@ import { TextInput, TextInputProps } from "react-native";
 import { styles } from "./styles";
 
 export function Input({ ...rest }: TextInputProps) {
-  return <TextInput {...rest} style={styles.container} />;
+  return (
+    <TextInput
+      {...rest}
+      placeholderTextColor="#74798B"
+      style={styles.container}
+    />
+  );
 }
