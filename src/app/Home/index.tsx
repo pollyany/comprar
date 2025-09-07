58594755d3c4aae23da1c1ff1194a163f84046a3
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-07 14:21:36 -0300

    Componente Filter

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,13 +2,21 @@ import { Image, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { FilterStatus } from "@/types/FilterStatus";
+import { Filter } from "@/components/Filter";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Adicionar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
