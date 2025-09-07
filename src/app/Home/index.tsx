9b8d3f50b949ac61570a474eca61cd7e9f593235
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-07 14:11:37 -0300

    Componente de Input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,13 +1,14 @@
 import { Image, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
-
-      <Button />
+      <Input placeholder="O que você precisa comprar?" />
+      <Button title="Adicionar" />
     </View>
   );
 }
